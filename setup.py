eeb35168360d151e60121362f5b12731b20ca27e
Martin Hosken <martin_hosken@sil.org>
2017-01-18 09:51:30 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+except ImportError:
+    import ez_setup
+    ez_setup.use_setuptools()
+    from setuptools import setup
+
+
+setup(
+    name = 'oxttools',
+    version =0.1,
+    description = 'Utility to make libreoffice language extensions',
+    maintainer = 'NRSI - SIL International',
+    url = 'http://github.com/silnrsi/oxttools',
+    packages = ["oxttools",
+        ],
+    package_dir = {'':'lib'},
+    install_requires=[
+        'lxml',
+    ],
+    package_data={
+        'oxttools': [
+             'data/*.xml',
+    ]},
+    scripts = ['scripts/makeoxt'],
+    license = 'MIT',
+    platforms = ['Linux','Win32','Mac OS X'],
+    classifiers = [
+        "Environment :: Console",
+        "Programming Language :: Python :: 2.7",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Text Processing :: Linguistic",
+        ],
+)
+
