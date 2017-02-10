239eb7634fd75e5ca6a89de56d98f750c63a06f4
Martin Hosken <martin_hosken@sil.org>
2017-02-10 13:01:16 +0100

    Add affix merging for all dictionary types

--- lib/oxttools/hunspell.py
+++ lib/oxttools/hunspell.py
@@ -7,16 +7,22 @@ class Hunspell(object) :
     def __init__(self, name) :
         self.name = name
         self.words = []
+        self.affix = ""
 
     def addword(self, word) :
         if len(word) :
             dat = unicodedata.normalize('NFC', word) 
             self.words.append(dat)
 
+    def mergeaffix(self, fname) :
+        if fname is not None :
+            with open(fname) as fd :
+                self.affix = "\n".join(fd.readlines())
+
     def getaff(self) :
         return """
 SET UTF-8
-"""
+""" + self.affix
 
     def getdic(self) :
         res = "{}\n".format(len(self.words))
