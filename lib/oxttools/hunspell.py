9ab47a2a8a1675e25d7df04d05795eb3d49484cb
Martin Hosken <martin_hosken@sil.org>
2017-05-22 17:10:54 +0100

    Add UCONV, WORDCHARS to .aff generation. Add -w command line option

--- lib/oxttools/hunspell.py
+++ lib/oxttools/hunspell.py
@@ -1,18 +1,37 @@
 
 import codecs
 import unicodedata
+import itertools
+
+def decompose(s) :
+    return "".join([unichr(int(x, 16)) for x in unicodedata.decomposition(s).split()])
+    
 
 class Hunspell(object) :
 
-    def __init__(self, name) :
+    def __init__(self, name, puncs="") :
         self.name = name
-        self.words = []
+        self.words = set()
         self.affix = ""
+        self.chars = set()
+        self.puncs = puncs
 
     def addword(self, word) :
         if len(word) :
-            dat = unicodedata.normalize('NFC', word) 
-            self.words.append(dat)
+            line = unicodedata.normalize('NFC', word)
+            for dat in line.split() :  # no phrases, just words
+                res = []
+                i = 0
+                while i < len(dat) :
+                    c = dat[i]
+                    while i+1 < len(dat) and unicodedata.combining(dat[i+1]) :
+                        i += 1
+                        c += dat[i]
+                    if ord(c[0]) > 128 or unicodedata.category(c[0])[0] not in "SP" or c[0] in self.puncs :
+                        self.chars.add(c)
+                        res.append(c)
+                    i += 1
+                self.words.add(u"".join(res))
 
     def mergeaffix(self, fname) :
         if fname is not None :
@@ -20,9 +39,41 @@ class Hunspell(object) :
                 self.affix = "\n".join(fd.readlines()).replace(u"\uFEFF", "")
 
     def getaff(self) :
-        return """
+        res = u"""
 SET UTF-8
 """ + self.affix
+
+        # calculate uconvs
+        uconv = []
+        specialchars = []
+        for c in self.chars :
+            r = c[0]
+            if unicodedata.category(r)[0] in 'SP' :
+                specialchars.append(r)
+            d = decompose(r)
+            if len(d) :
+                uconv.append((d, r))
+                i = 0
+                for i in range(1, len(c)) :
+                    if unicodedata.combining(c[i]) :
+                        d += c[i]
+                        r += c[i]
+                        for p in itertools.permutations(d[1:]) :
+                            t = (u"".join(p), r)
+                            if t not in uconv :
+                                uconv.append(t)
+                    else :
+                        break
+
+        if len(specialchars) :
+            res += u"\nWORDCHARS {}\n".format(u"".join(specialchars))
+
+        if len(uconv) :
+            res += u"\nICONV {}\n".format(len(uconv))
+            for u in uconv :
+                res += u"ICONV {} {}\n".format(u[0], u[1])
+        return res
+
 
     def getdic(self) :
         res = "{}\n".format(len(self.words))
